cae77ceb21bb15f5c278249d28830d95c0f8653b
Diana Verevkina <diana_verevkina@mail.ru>
2023-05-13 21:45:06 +0300

    added footer

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ icon.addEventListener('click', clickMenuBurger);
 
 //Фиксирование header и смена background 
 function fixHeader() {
-  // debugger;
   if (this.scrollY > 30) {
     headerElement.classList.add('header_fixed');
     menuItems.classList.add('menu__items_fixed');
@@ -27,14 +26,17 @@ if (document.documentElement.clientWidth < 1000) {
   window.addEventListener('scroll', fixHeader);
 }
 
+const treasures = document.querySelector('.treasures');
+const treasuresList = treasures.querySelectorAll('.treasure');
 
-//Слайдер
+if (document.documentElement.clientWidth < 768) {
+  for (let i = 3; i < treasuresList.length; i++){
+    treasuresList[i].remove(); 
+  }
+}
+
+//Слайдер для блока Creators
 let swiper = new Swiper('.creators__slider', {
-  // navigation: {
-  //    nextEl: '.swiper-button-next',
-  //    prevEl: '.swiper-button-prev',
-  // },
-
   keybord: {
      enabled: true,
      onlyInViewport: true,
@@ -43,19 +45,16 @@ let swiper = new Swiper('.creators__slider', {
 
   speed: 1500,
   centeredSlides: true,
-  // slidesPerView: 4.2,
   slidesPerView: 'auto',
   grabCursor: true,
   
   autoplay: {
     delay: 800,
     disableOnInteraction: true,	
-    // pauseOnMouseEnter: true,
+    pauseOnMouseEnter: true,
   },
 
   slideToClickedSlide: true,
-  watchOverflow: false,
-
   loop: true,
 
   effect: 'coverflow',
@@ -68,22 +67,9 @@ let swiper = new Swiper('.creators__slider', {
 		},
 })
 
-// let slides = document.querySelectorAll('.creator-card');
-// slides.forEach(slide => {
-//   if (slide.classList.contains('swiper-slide-active')){
-//     let buttons = slide.querySelectorAll('.creator-btn');
-//     buttons.forEach(button => {
-//       button.classList.add('creator-btn_active');
-//     })
-    
-//   }
-// })
-
 const subscriptionForm = document.querySelector('.subscription__form');
-const subscriptionField = subscriptionForm.querySelector('.subscription__input-wrapper');
 const subscriptionText = subscriptionForm.querySelector('.subscription__input-text');
 const subscriptionInput = subscriptionForm.querySelector('.subscription__input');
-const subscriptionBtn = subscriptionForm.querySelector('.subscription__button');
 
 function changeInput() {
   subscriptionText.classList.add('subscription__input-text_hidden');
@@ -91,6 +77,7 @@ function changeInput() {
   subscriptionInput.focus();
 }
 
+//Отправка мейла для подписки
 function submitEmail (e) {
   e.preventDefault();
   subscriptionInput.classList.remove('subscription__input_visible');
@@ -101,31 +88,58 @@ subscriptionText.addEventListener('click', changeInput);
 subscriptionForm.addEventListener('submit', submitEmail);
 
 
+//Слайдер верхний для блока Companions
 let companionsSliderUpper = new Swiper('.slider-companions_upper', {
   autoplay: {
     delay: 0,
 		disableOnInteraction: false, 
 		reverseDirection: true,
-    waitForTransition: false, 
   },
+
   loop: true,
-	spaceBetween: 18,
+	spaceBetween: 12,
   slidesPerView: 'auto',
   speed: 4000,
-  // waitForTransition: false,
+  allowTouchMove: false, 
+
+  breakpoints: {
+    992: {
+      spaceBetween: 18,
+    },
+  },
 });
 
+//Слайдер нижний для блока Companions
 let companionsSliderLower = new Swiper('.slider-companions_lower', {
   autoplay: {
     delay: 0, 
 		disableOnInteraction: false,
   },
+
   loop: true, 
-	spaceBetween: 10,
+	spaceBetween: 12,
   slidesPerView: 'auto',
   speed: 4000, 
   allowTouchMove: false, 
-  waitForTransition: false,
+
+  breakpoints: {
+    992: {
+      spaceBetween: 18,
+    },
+  },
+});
+
+const footer = document.querySelector('.footer');
+const footerMenuItems = footer.querySelectorAll('.footer-menu__item');
+
+//Открытие подменю в footer
+function showSubmenu() {
+  let footerSubmenu = this.querySelector('.footer-submenu');
+  footerSubmenu.classList.toggle('footer-submenu_active');
+}
+
+footerMenuItems.forEach(item => {
+  item.addEventListener('click', showSubmenu);
 })
 
 
